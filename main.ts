4810cd7ceddc0368ad50934e4bdd8d54cd3fd4a9
forrest1398 <129135885+forrest1398@users.noreply.github.com>
2024-12-20 20:00:07 +0900

    ✨ Feat : 현재 Editor의 내용을 출력하는 Modal 구현
    Editor의 Markdown 텍스트를 HTML Element로 렌더링해서 출력한다.

--- main.ts
+++ main.ts
@@ -4,10 +4,9 @@ import {
 	Notice,
 	Plugin,
 	PluginSettingTab,
-	Setting,
+	MarkdownRenderer,
+	Component,
 } from "obsidian";
-
-//TODO : Remember to rename these classes and interfaces!
 
 interface EasyTestPluginSettings {}
 
@@ -36,19 +35,32 @@ export default class EasyTestPlugin extends Plugin {
 
 	async onload() {
 		await this.loadSettings();
+		this.addSettingTab(new EasyTestPluginSettingTab(this.app, this));
 
-		// Add ribbonIcon creating modal
-		const ribbonIconEl = this.addRibbonIcon(
-			"ribbon",
-			"Make Test",
+		// Add ribbonIcon creating test
+		const openTestIcon = this.addRibbonIcon(
+			"notebook-pen",
+			"Open Test",
 			(evt: MouseEvent) => {
-				new Notice("Hello, world");
+				const editor = this.app.workspace.activeEditor?.editor;
+				if (!editor) {
+					new Notice("No active editor found.");
+					return;
+				}
+
+				const doc = editor.getDoc?.();
+				if (!doc) {
+					new Notice("The editor does not have a valid document.");
+					return;
+				}
+
+				const markdownContent = doc.getValue() || "";
+
+				new TestModal(this.app, markdownContent).open();
 			}
 		);
 
-		this.addSettingTab(new EasyTestPluginSettingTab(this.app, this));
-
-		// 아직 학습하지 못한 코드
+		//------------------------------------ 아직 학습하지 못한 코드 ---------------------------------------------
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
 		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
@@ -72,5 +84,30 @@ export default class EasyTestPlugin extends Plugin {
 	}
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+}
+
+class TestModal extends Modal {
+	component: Component;
+	content: string;
+
+	constructor(app: App, content: any) {
+		super(app);
+		this.content = content;
+		this.component = new Component();
+	}
+	onOpen() {
+		this.component.load();
+		MarkdownRenderer.render(
+			this.app,
+			this.content,
+			this.contentEl,
+			"",
+			this.component
+		);
+	}
+
+	onClose() {
+		this.component.unload();
 	}
 }
